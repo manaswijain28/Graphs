304d152f3753282922a359018fa525476944d0be
manaswijain28 <tiyajain732@gmail.com>
2025-06-13 13:20:47 +0530

    Create 25lexSmallestTopoOrder.cpp

---
+++ 25lexSmallestTopoOrder.cpp
@@ -0,0 +1,59 @@
+
+void solve(){
+
+    int n,m;
+    cin>>n>>m;
+
+    vector<vector<int>> adj(n+1);
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+    }
+
+    vector<int> topo_order;
+    vector<int> indegree(n+1);
+
+    for(int i=1;i<=n;i++){
+        for(auto it : adj[i]){
+            indegree[it]++;
+        }
+    }
+
+    //if there is no node with indegree as 0 it means that 
+    //the graph contains cycle..
+
+    priority_queue<int,vector<int> ,greater<>> q;
+
+    for(int i=1;i<=n;i++){
+        if(indegree[i] == 0){
+            q.push(i);
+        }
+    }
+
+    while(!q.empty()){
+        int u = q.top();
+        q.pop();
+
+        topo_order.push_back(u);
+
+        for(auto it : adj[u]){
+            indegree[it]--;
+            if(indegree[it] == 0){
+                q.push(it);
+            }
+        }
+    }
+
+    for(int i=0;i<topo_order.size();i++){
+        cout<<topo_order[i]<<" ";
+    }
+
+    cout<<endl;
+    //O(v+e)
+    
+
+
+    
+
+}
